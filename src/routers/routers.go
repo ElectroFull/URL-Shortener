54fbcf396da1a7d7b4048795bd91b632eee4aa50
ElectroFull <53854140+ElectroFull@users.noreply.github.com>
2025-07-25 20:20:38 +0500

    Add basic router implementation to register HTTP handlers

--- src/routers/routers.go
+++ src/routers/routers.go
@@ -1,1 +1,10 @@
 package routers
+
+import (
+    "github.com/gofiber/fiber/v2"
+	"github.com/electrofull/URL-Shortener/src/handlers"
+)
+
+func RegisterHandlers(app *fiber.App) {
+	app.Get("/", handlers.Welcome)
+}
