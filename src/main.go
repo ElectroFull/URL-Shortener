acf6c8e5769c00f3b2930afa9a85ef8467d1aa87
ElectroFull <53854140+ElectroFull@users.noreply.github.com>
2025-07-25 20:20:38 +0500

    Refactor main.go to modularize environment loading, middleware setup, and server initialization

--- src/main.go
+++ src/main.go
@@ -1,21 +1,55 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"os"
-
+	"github.com/electrofull/URL-Shortener/src/routers"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/joho/godotenv"
 )
 
-func main() {
+
+func loadEnvironment(){
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file!")
 	}
+}
+
+func getPort() string {
 	portString := os.Getenv("PORT")
 	if portString == "" {
-		log.Fatal("PORT environment variable is not set")
+		log.Fatal("PORT environment variable is not set!")
 	}
-	fmt.Println("Server will run on port:", portString)
+	return portString
+}
+
+func setupMiddleware(app *fiber.App) {
+	app.Use(logger.New(logger.Config{
+    	Format: "\033[32mINFO:\033[0m 	[${ip}]:${port}   ${status} - ${method} ${path}\n",
+	})) // Just logs the basic info (requests)
+
+	app.Use(recover.New(recover.Config{
+		EnableStackTrace: true,
+	})) // Catches an error and shows where the error happened.
+
+	
+}
+
+func main() {
+	loadEnvironment()
+
+	portString := getPort()
+
+	app := fiber.New()
+
+	setupMiddleware(app)
+	
+	log.SetLevel(log.LevelDebug)
+
+	routers.RegisterHandlers(app)
+
+	log.Fatal(app.Listen(":" + portString))
 }
