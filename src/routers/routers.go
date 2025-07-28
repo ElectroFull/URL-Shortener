9d27d880bcaf06f51a884afe74a5b2f76eb2ced5
ElectroFull <53854140+ElectroFull@users.noreply.github.com>
2025-07-29 00:01:39 +0500

    Enhance routers to include user registration and URL shortening endpoints

--- src/routers/routers.go
+++ src/routers/routers.go
@@ -3,8 +3,18 @@ package routers
 import (
     "github.com/gofiber/fiber/v2"
 	"github.com/electrofull/URL-Shortener/src/handlers"
+	"github.com/jackc/pgx/v5/pgxpool"
+	jwtware "github.com/gofiber/contrib/jwt"
+	"os"
 )
 
-func RegisterHandlers(app *fiber.App) {
+func RegisterHandlers(app *fiber.App, conn *pgxpool.Pool) {
+	jwt_secret := os.Getenv("JWT_SECRET")
 	app.Get("/", handlers.Welcome)
+	app.Post("/register", handlers.Register(conn, jwt_secret))
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{Key: []byte(jwt_secret)},
+	})) // JWT Authentication
+	app.Post("/shorten", handlers.Shorten(conn))
+	
 }
