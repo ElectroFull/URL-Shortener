f6e25b939a830d20289900eac9a100d817d3fd71
ElectroFull <53854140+ElectroFull@users.noreply.github.com>
2025-07-29 00:02:53 +0500

    Add Base62 encoding utility function

---
+++ src/helpers/base62.go
@@ -0,0 +1,15 @@
+package helpers
+
+const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+func Base62Encode(number int) string {
+	var encoding []byte
+	for number > 0 {
+		encoding = append(encoding, alphabet[number % 62])
+		number /= 62
+	}
+	for i, j := 0, len(encoding) - 1; i < j; i, j = i + 1, j - 1 {
+		encoding[i], encoding[j] = encoding[j], encoding[i]
+	}
+	return string(encoding)
+}
