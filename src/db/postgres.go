abdcc85bf615a856f80dd8cfc0944ef591848a10
ElectroFull <53854140+ElectroFull@users.noreply.github.com>
2025-07-29 18:49:28 +0500

    Add database migration functionality to ensure schema is initialized

--- src/db/postgres.go
+++ src/db/postgres.go
@@ -12,7 +12,31 @@ func SetupConnection() *pgxpool.Pool{
 
 	conn, err := pgxpool.New(context.Background(), URL)
 	if err != nil {
-		log.Fatalf("Failed to connect to database", err)
+		log.Fatalf("Failed to connect to database: %v", err)
 	}
+
+	err = runMigrations(conn)
+
+	if err != nil {
+		log.Fatalf("Failed to run database migrations: %v", err)
+	}
+
 	return conn
+}
+
+func runMigrations(db *pgxpool.Pool) error {
+	sql_lines, err := os.ReadFile("../tables.sql")
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(context.Background(), string(sql_lines))
+
+	if err != nil {
+		return err
+	}
+
+	log.Info("Database schema initialized successfully")
+
+	return nil
 }
